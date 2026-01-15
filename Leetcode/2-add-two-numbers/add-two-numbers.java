37762d530729a92891b06bb37bf2dc5953395689
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-01-16 02:59:14 +0530

    Time: 1 ms (99.78%) | Memory: 46.6 MB (24.59%) - LeetSync

--- Leetcode/2-add-two-numbers/add-two-numbers.java
+++ Leetcode/2-add-two-numbers/add-two-numbers.java
@@ -8,28 +8,31 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.util.*;
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head =new ListNode();
-        ListNode current =head;
-        int sum=0;
-        while(l1!=null||l2!=null|| sum!=0){
-            
-            if (l1!=null){
-                sum+=l1.val;
-                l1=l1.next;
-            }
-            if(l2!=null){
-                sum+=l2.val;
-                l2=l2.next;                
-            }
-            ListNode newnode= new ListNode(sum%10);
-            sum=sum/10;
-            
-            current.next=newnode;
-            current =current.next;
-        }
-        return head.next;
+    int sum=0;
+    ListNode curr1=l1;
+    ListNode curr2=l2;
+    ListNode head3=new ListNode();
+    ListNode curr3=head3;
+     while (curr1 !=null||curr2!=null||sum!=0){
+         int a=0,b=0;
+         ListNode newNode =new ListNode ();
+         if (curr1!=null){
+             a=curr1.val;
+             curr1=curr1.next;
+         }
+         if (curr2!=null){
+             b=curr2.val;
+             curr2=curr2.next;
+         }
+         sum+=(a+b);
+         newNode.val=sum%10;
+         sum/=10;
+         curr3.next=newNode ;
+         curr3=curr3.next;
+     }
+      return head3.next;
+        
     }
 }
