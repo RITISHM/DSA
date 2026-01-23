0e2acaa3671cadac354f3c5ed83ffb6ec4b7b2dc
Ritish <70802345+RITISHM@users.noreply.github.com>
2026-01-23 17:16:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (87.81%) - LeetSync

--- Leetcode/35-search-insert-position/search-insert-position.java
+++ Leetcode/35-search-insert-position/search-insert-position.java
@@ -1,15 +1,14 @@
 class Solution {
-    
-    public int searchInsert(int[] arr, int k) {
-       int left=0;
-       int right=arr.length-1;
-       while(left<=right){
-        int mid=left+(right-left)/2;
-        if(arr[mid]>=k){
-            right=mid-1;
+    public int searchInsert(int[] nums, int target) {
+        int ans =0;
+        int left =0;
+        int right =nums.length-1;
+        while (left <=right ){
+            int mid =left +(right -left)/2;
+            if (nums[mid]<target)ans=mid+1;
+            if (nums[mid]<target)left=mid+1;
+            else right =mid -1;
         }
-        else left=mid+1;
-       }
-       return left;
+        return ans ;
     }
 }
